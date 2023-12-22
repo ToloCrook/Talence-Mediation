407efeb6d41941709d835d32b021ed2300e2e202
Tolotra Ramarovahoaka <tolotraramaro@hotmail.fr>
2023-12-22 11:58:21 +0100

    first three blocks

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/navbar.css';
+import './styles/greenBackground.css';
+import './styles/secondBlock.css';
+import './styles/thirdBlock.css';
