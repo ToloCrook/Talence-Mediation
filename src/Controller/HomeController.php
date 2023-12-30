3db0ab3273d5e6913da1e32ef36f47bbafca63ff
Tolotra Ramarovahoaka <tolotraramaro@hotmail.fr>
2023-12-31 00:23:37 +0100

    handling form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +12,35 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/form', name: 'app_form', methods: 'POST')]
+    public function form(Request $request): Response
+    {
+
+        if ($request->isMethod('POST')) {
+            $message = $request->get('message');
+            $email = $request->get('email');
+            $cleanEmail = filter_var($email, FILTER_SANITIZE_EMAIL);
+            $object = $request->get('object');
+
+            if (empty($message) || empty($email) || empty($object)) {
+                $this->addFlash('warning', "Veuillez remplir correctement le formulaire en indiquant un email, un objet et un message.");
+
+                return $this->redirectToRoute('app_home');
+            }
+
+            if ($email == $cleanEmail && filter_var($email, FILTER_VALIDATE_EMAIL) && preg_match("/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/", $email)) {
+                $this->addFlash('success', "Votre message a bien été envoyé.");
+
+                return $this->redirectToRoute('app_home');
+            }
+            $this->addFlash('warning', "Le format d'email renseigné n'est pas correct.");
+        }
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
